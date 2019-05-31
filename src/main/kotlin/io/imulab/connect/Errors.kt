9bf3d840ad48ef5e21b5dad6297a09f73bfab14f
Weinan Qiu <davidiamyou@gmail.com>
2019-05-31 12:29:07 +0800

    #9 add validating parser

--- src/main/kotlin/io/imulab/connect/Errors.kt
+++ src/main/kotlin/io/imulab/connect/Errors.kt
@@ -38,7 +38,7 @@ object Errors {
 
     fun unsupportedResponseType(responseType: ResponseType): ConnectException = ConnectException(
         error = Codes.unsupportedResponseType,
-        description = "response_type ${responseType.value} is not supports.",
+        description = "response_type '${responseType.value}' is not supported.",
         statusCode = 400
     )
 
